ac88b4f551168148ff5e5421e2ec668b80d30523
Brokkoly <brendankrull@gmail.com>
2017-12-20 17:43:57 -0600

    magic numbers

--- main.cu
+++ main.cu
@@ -26,30 +26,35 @@ int main(int argc,char** argv){
     float* da;
     cudaMalloc((void**)&da,sizeof(float)*26);
     cudaMemcpy(da,a,sizeof(float)*26,cudaMemcpyHostToDevice);
-    matrixReductionDestructive<<<2,13,13*sizeof(float)>>>(da,13,16);
+    matrixReductionDestructive<<<2,13,13*sizeof(float)>>>(da,13,hibit(13)<<1);
     cudaMemcpy(a,da,sizeof(float)*26,cudaMemcpyDeviceToHost);
 
     printf("Device Results: %f,%f\nHost Results: %f,%f\n",a[0],a[13],b[0],b[1]);
     cudaFree(da);
     free(a);
     free(b);
-
-
-
+}
+int hibit(unsigned int n) {
+    n |= (n >>  1);
+    n |= (n >>  2);
+    n |= (n >>  4);
+    n |= (n >>  8);
+    n |= (n >> 16);
+    return n - (n >> 1);
 }
 
 void trainingInstance(float* dx,float* dh, float* dy,float* dyCorrect,float* ddels,float* dgammas,float* dinter,float** dWeights,float** ddeltas,int numX,int numH,int numY,float alpha,float lrate,int dinterSize){
 
     //firstLayer
     forwardPropagation<<<numH,numX>>>(dx,dinter,dWeights[0],dinterSize);
-    matrixReduction<<<numH,numX,numX*sizeof(float)>>>(dinter,dh,1024,1024);
+    matrixReduction<<<numH,numX,numX*sizeof(float)>>>(dinter,dh,1024,hibit(1024));
     sigmoidKernel<<<1,numH>>>(dh);
 
     //first layer done
 
     //second layer:
     forwardPropagation<<<numY,numH>>>(dh,dinter,dWeights[1],dinterSize);
-    matrixReduction<<<numY,numH,numH*sizeof(float)>>>(dinter,dy,1024,1024);
+    matrixReduction<<<numY,numH,numH*sizeof(float)>>>(dinter,dy,1024,hibit(1024));
     sigmoidKernel<<<1,numY>>>(dy);
 
     //second layer done
@@ -60,7 +65,7 @@ void trainingInstance(float* dx,float* dh, float* dy,float* dyCorrect,float* dde
     backPropagationFirstKernel<<<numY,numH>>>(dh,dy,dyCorrect,dWeights[1],ddeltas[1],ddels,alpha,lrate);
     //dim3 grid(numY,numH);
     backPropagationSecondKernelPart1<<<numY,numH>>>(dh,dgammas,dWeights[1],ddels,alpha,lrate);
-    matrixReduction<<<numH,numY,numY*sizeof(float)>>>(dgammas,dgammas);
+    matrixReduction<<<numH,numY,numY*sizeof(float)>>>(dgammas,dgammas,numY,hibit(numY));
     backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas[0],alpha,lrate);
 
 
