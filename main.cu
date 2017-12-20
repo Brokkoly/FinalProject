74dea907b70a848bb682d2f2875d888d40431695
Brokkoly <brendankrull@gmail.com>
2017-12-20 17:01:13 -0600

    blah

--- main.cu
+++ main.cu
@@ -42,7 +42,7 @@ void trainingInstance(float* dx,float* dh, float* dy,float* dyCorrect,float* dde
     //dim3 grid(numY,numH);
     backPropagationSecondKernelPart1<<<numY,numH>>>(dh,dgammas,dWeights[1],ddels,alpha,lrate);
     matrixReduction<<<numH,numY,numY*sizeof(float)>>>(dgammas,dgammas);
-    backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas[1],alpha,lrate);
+    backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas[0],alpha,lrate);
 
 
 
