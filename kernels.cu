8b79c6593c7b2d765fdc0c900340f3abed92944d
Brokkoly <brendankrull@gmail.com>
2017-12-20 16:37:40 -0600

    may be done with a training instance

--- kernels.cu
+++ kernels.cu
@@ -19,16 +19,12 @@ __device__ float sigmoidDerivative(float x){
     float val = sigmiod(x);
     return val*(1-val);
 }
-__global__ void forwardPropagationKernel(float* input,float* weights,float* output,int n, int m){
-    int windex = threadIDx.x+blockIDx.x*blockDim.x;
-    int vindex = threadIDx.x;
-    output[windex] = input[vindex]+weights[windex];
-}
+
 __global__ void applySigmoid(float* hiddenLayer){
     int index = threadIDx.x;
     hiddenLayer[index] = sigmoid(hiddenLayer[index]);
 }
-__global__ void backPropagationFirstKernel(float* inputLayer,float* outputLayer,float* outputLayerTrue,float* weights,float* deltas,float* dels,int iSize,int oSize,float alpha,float lrate){
+__global__ void backPropagationFirstKernel(float* inputLayer,float* outputLayer,float* outputLayerTrue,float* weights,float* deltas,float* dels,float alpha,float lrate){
     int tindex = threadIDx.x;
     int oindex = blockIDx.x;
     int windex = tindex+blockIDx.x*blockDim.x;
@@ -38,27 +34,28 @@ __global__ void backPropagationFirstKernel(float* inputLayer,float* outputLayer,
 }
 
 //blocks: (# of output layers,# of hidden layers)
-__global__ void backPropagationSecondKernelPart1(float* hiddenLayer,float* gammas,float* weights,float* dels,int xSize,int hSize,int oSize,float alpha,float lrate){
+__global__ void backPropagationSecondKernelPart1(float* hiddenLayer,float* gammas,float* weights,float* dels,float alpha,float lrate){
     int tindex = threadIDx.x;//For each hidden node
     int oindex = blockIDx.x;//for each output node
-    int mindex = blockIDx.y;//for each hidden layer
+    //int mindex = blockIDx.y;//for each hidden layer
     int windex = tindex+blockIDx.x*blockDim.x;
     //int sindex = tindex+blockIDx.y*gridDim.x*blockDim.x;
     //float gamma = 0;
-    gammas[gindex] = dels[oindex]*weights[windex]*h[mindex]*(1-h[mindex]);
+    gammas[gindex] = dels[oindex]*weights[windex]*h[tindex]*(1-h[tindex]);
     //float del = (outputLayer[oindex]-outputLayerTrue[oindex])*outputLayer[oindex]*(1-outputLayer[oindex]);
     //deltas[windex] = (1-alpha)*lrate*del*inputLayer[tindex]+alpha*deltas[windex];
     //weights[windex] = weights[windex]-deltas[windex];
 }
-__global__ void backPropagationSecondKernelPart1(float* gammas,int kSize){
+/*
+__global__ void backPropagationSecondKernelPart15(float* gammas,int kSize){
     int tindex = threadIDx.x;//For each hidden node
     int gindex = tindex*blockDim.x;
     for(int i = 1; i < kSize;i++){
         gammas[gindex]+=gammas[gindex+i];
     }
-}
+}*/
 
-__global__ void backPropagationSecondKernelPart2(float* inputLayer,float* gammas,float* weights,float* deltas,float alpha,float lrate){
+__global__ void backPropagationSecondKernelPart2(float* inputLayer,float* gammas,float* weights,float* deltas,float* alpha,float lrate){
     int tindex = threadIDx.x;//For each upper node
     int oindex = blockIDx.x;//for each hidden node
     int windex = tindex+blockIDx.x*blockDim.x;//
@@ -67,9 +64,13 @@ __global__ void backPropagationSecondKernelPart2(float* inputLayer,float* gammas
 }
 __global__ void forwardPropagation(float* x,float*Y,float* W,int yWidth){
     int tindex = threadIDx.x;
-    int yindex = threadIDx.x+blockIDx.x*yWidth;
+    int yindex = threadIDx.x+blockIDx.x*yWidth;//yWidth different because it has to be an exponent of 2
     int windex = threadIDx.x+blockIDx.x*blockDim.x;
     Y[yindex] = x[tindex]*W[windex];
+    //then perform reduction,then sigmoid
+}
+__global__ void sigmoidKernel(float* y){
+    y[threadIDx.x] = sigmoid(y[threadIDx.x]);
 }
 
 __global__ void matrixReduction(float *g_data,float *out_data)
@@ -88,31 +89,36 @@ __global__ void matrixReduction(float *g_data,float *out_data)
     }
     if(tindex==0) out_data[blockIdx.x] = sdata[0];
 }
-
-__global__ void firstStep(Matrix X, Matrix H,Matrix Inter,Matrix Wf,Matrix Wi,Matrix Wc,Matrix ft,Matrix it,Matrix Ct1,Matrix ot){
-
-    int threadidx = threadIDx.x;// N threads, N values in output
-    int blockId = blockIDx.x;
-    int t = blockIDx.y;
-    int function = blockIDx.y;
-    int Tindex = t*blockDim.x*blockDim.y;
-    int Iindex = blockId*blockDim.x*blockDim.y;
-    int Windex = threadidx + blockIDx.x*blockDim.x+block
-    if(function==0){
-        //f_t
-        for(int i=0; i < N;i++){
-
-            Inter[Iindex]=Wf[i]
+__global__ void matrixReductionDestructive(float *g_data)
+{
+    extern __shared__ float sdata[];
+    unsigned int tindex = (threadIdx.x);
+    unsigned int i = blockIdx.x*blockDim.x+threadIdx.x;
+    sdata[tindex] = g_data[i];
+    __syncthreads();
+    for(unsigned int s = blockDim.x/2;s>=0;s>>=1){
+        //int index = 2*s*tindex;
+        if((tindex<(s+1))&&((tindex+s+1)<blockDim.x)){
+            sdata[tindex]+=sdata[tindex+s+1];
         }
-
-
+        __syncthreads();
     }
-    //Things to calculate:
-    //f_t
-    //i_t
-    //C_t
-    //o_t
-
-
-
+    if(tindex==0) g_data[blockIdx.x] = sdata[0];
 }
+__global__ void matrixReduction(float *g_data,float* o_data)
+{
+    extern __shared__ float sdata[];
+    unsigned int tindex = (threadIdx.x);
+    unsigned int i = blockIdx.x*blockDim.x+threadIdx.x;
+    sdata[tindex] = g_data[i];
+    __syncthreads();
+    for(unsigned int s = blockDim.x/2;s>=0;s>>=1){
+        //int index = 2*s*tindex;
+        if((tindex<(s+1))&&((tindex+s+1)<blockDim.x)){
+            sdata[tindex]+=sdata[tindex+s+1];
+        }
+        __syncthreads();
+    }
+    if(tindex==0) o_data[blockIdx.x] = sdata[0];
+}
+
---
+++ main.cu
@@ -0,0 +1,49 @@
+
+
+
+int main(int argc,char* argv){
+
+    //Initialize weight matrices
+
+    //get inputs from training file
+    //get inputs from test file
+
+
+
+
+
+
+
+}
+
+void trainingInstance(float* dx,float* dh, float* dy,float* dyCorrect,float* ddels,float* dgammas,float* dinter,float** dWeights,float** ddeltas,int numX,int numH,int numY,float alpha,float lrate,int dinterSize){
+
+    //firstLayer
+    forwardPropagation<<<numH,numX>>>(dx,dinter,dWeights[0],dinterSize);
+    matrixReduction<<<numH,numX,numX*sizeof(float)>>>(dinter,dh);
+    sigmoidKernel<<<numH>>>(dh);
+
+    //first layer done
+
+    //second layer:
+    forwardPropagation<<<numY,numH>>>(dh,dinter,dWeights[1],dinterSize);
+    matrixReduction<<<numY,numH,numH*sizeof(float)>>>(dinter,dy);
+    sigmoidKernel<<<numY>>>(dy);
+
+    //second layer done
+
+    //backpropagation:
+    
+
+    backPropagationFirstKernel<<<numY,numH>>>(dh,dy,dyCorrect,dWeights[1],ddeltas,ddels,alpha,lrate);
+    //dim3 grid(numY,numH);
+    backPropagationSecondKernelPart1<<<numY,numH>>>(dh,dgammas,dWeights[1],ddels,alpha,lrate);
+    matrixReduction<<<numH,numY,numY*sizeof(float)>>(dgammas,dgammas);
+    backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas,alpha,lrate)
+
+
+
+
+
+
+}
