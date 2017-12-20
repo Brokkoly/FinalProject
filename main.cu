1bcc4902c0b47857e0b8b14889756bfcf305c3c0
Brokkoly <brendankrull@gmail.com>
2017-12-20 16:57:34 -0600

    still need to figure out sigmoid

--- main.cu
+++ main.cu
@@ -24,25 +24,25 @@ void trainingInstance(float* dx,float* dh, float* dy,float* dyCorrect,float* dde
     //firstLayer
     forwardPropagation<<<numH,numX>>>(dx,dinter,dWeights[0],dinterSize);
     matrixReduction<<<numH,numX,numX*sizeof(float)>>>(dinter,dh);
-    sigmoidKernel<<<numH>>>(dh);
+    sigmoidKernel<<<1,numH>>>(dh);
 
     //first layer done
 
     //second layer:
     forwardPropagation<<<numY,numH>>>(dh,dinter,dWeights[1],dinterSize);
     matrixReduction<<<numY,numH,numH*sizeof(float)>>>(dinter,dy);
-    sigmoidKernel<<<numY>>>(dy);
+    sigmoidKernel<<<1,numY>>>(dy);
 
     //second layer done
 
     //backpropagation:
     
 
-    backPropagationFirstKernel<<<numY,numH>>>(dh,dy,dyCorrect,dWeights[1],ddeltas,ddels,alpha,lrate);
+    backPropagationFirstKernel<<<numY,numH>>>(dh,dy,dyCorrect,dWeights[1],ddeltas[1],ddels,alpha,lrate);
     //dim3 grid(numY,numH);
     backPropagationSecondKernelPart1<<<numY,numH>>>(dh,dgammas,dWeights[1],ddels,alpha,lrate);
     matrixReduction<<<numH,numY,numY*sizeof(float)>>>(dgammas,dgammas);
-    backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas,alpha,lrate)
+    backPropagationSecondKernelPart2<<<numH,numX>>>(dx,dgammas,dWeights[0],ddeltas[1],alpha,lrate);
 
 
 
