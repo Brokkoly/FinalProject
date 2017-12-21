c19d25ffc9736b4240c84dac1b9ba26cf5d42374
Brokkoly <brendankrull@gmail.com>
2017-12-20 21:21:17 -0600

    added debug line

--- main.cu
+++ main.cu
@@ -40,6 +40,7 @@ double* generateRandomWeights(int size){
 }
 
 void printArr(double* arr,int rows,int cols){
+    printf("PRINTING NEW ARRAY!\n");
     for(int i = 0; i < rows;i++){
         for(int j = 0; j < cols;j++){
             printf(" %lf ", arr[i*cols+j] );
